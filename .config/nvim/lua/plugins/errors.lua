a63dad491dfab3c782757fe9cfc42a16bd28a1d9
iz7n <48165199+iz7n@users.noreply.github.com>
2024-06-18 13:04:36 -0400

    lazily start trouble.nvim

--- .config/nvim/lua/plugins/errors.lua
+++ .config/nvim/lua/plugins/errors.lua
@@ -1,13 +1,16 @@
 return {
-    {
-        "folke/trouble.nvim",
-        event = "BufRead",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        opts = {},
-        init = function()
-            vim.keymap.set("n", "<leader>tt", function()
-                require("trouble").toggle()
-            end)
-        end,
-    },
+	{
+		"folke/trouble.nvim",
+		event = "BufRead",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>tt",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+		},
+		opts = {},
+	},
 }
