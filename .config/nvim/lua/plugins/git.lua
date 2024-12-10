b9f95337db7c02a58323f24622e5e9b05cfe1586
iz7n <48165199+iz7n@users.noreply.github.com>
2024-12-10 00:50:40 -0500

    chore(nvim): fmt git.lua

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,30 +1,30 @@
 return {
-    {
-        "lewis6991/gitsigns.nvim",
-        event = "BufReadPre",
-        opts = {
-            signs = {
-                add = { text = "+" },
-                change = { text = "~" },
-                delete = { text = "_" },
-                topdelete = { text = "‾" },
-                changedelete = { text = "~" },
-            },
-        },
-    },
-    {
-        "sindrets/diffview.nvim",
-        keys = {
-            { "<leader>gd", "<cmd>DiffviewOpen<cr>" },
-        },
-    },
-    {
-        "kdheepak/lazygit.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-        },
-        keys = {
-            { "<leader>lg", "<cmd>LazyGit<cr>" },
-        },
-    },
+	{
+		"lewis6991/gitsigns.nvim",
+		event = "BufReadPre",
+		opts = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+		},
+	},
+	{
+		"sindrets/diffview.nvim",
+		keys = {
+			{ "<leader>gd", "<cmd>DiffviewOpen<cr>" },
+		},
+	},
+	{
+		"kdheepak/lazygit.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		keys = {
+			{ "<leader>lg", "<cmd>LazyGit<cr>" },
+		},
+	},
 }
