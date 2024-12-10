97110b854896f1ab9099087de80b7b384fd6c6d5
iz7n <48165199+iz7n@users.noreply.github.com>
2024-12-10 00:49:35 -0500

    feat(nvim): remove unused telescope commands

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,25 +1,18 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	tag = "0.1.7",
+	tag = "0.1.8",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-		"nvim-telescope/telescope-ui-select.nvim",
 	},
 	config = function()
 		local telescope = require("telescope")
-		telescope.setup({
-			extensions = {
-				["ui-select"] = {
-					require("telescope.themes").get_dropdown(),
-				},
-			},
-		})
+		telescope.setup({})
 		telescope.load_extension("fzf")
-		telescope.load_extension("ui-select")
 
 		local builtin = require("telescope.builtin")
+		local themes = require("telescope.themes")
 		vim.keymap.set("n", "<C-f>", builtin.find_files, { desc = "Find [F]iles" })
 		vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[F]ind current [W]ord" })
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ile [G]rep" })
@@ -30,21 +23,9 @@ return {
 
 		vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[F]ind [D]iagnostics" })
 
-		vim.keymap.set("n", "<leader>fp", builtin.planets, { desc = "[F]ind [P]lanets" })
-
 		vim.keymap.set("n", "<leader>/", function()
-			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-				winblend = 10,
-				previewer = false,
-			}))
+			builtin.current_buffer_fuzzy_find(themes.get_dropdown())
 		end, { desc = "[/] Fuzzy search in current buffer" })
-
-		vim.keymap.set("n", "<leader>f/", function()
-			builtin.live_grep({
-				grep_open_files = true,
-				prompt_title = "Live Grep in Open Files",
-			})
-		end, { desc = "[F]ind [/] in Open Files" })
 
 		vim.keymap.set("n", "<leader>fn", function()
 			builtin.find_files({ cwd = vim.fn.stdpath("config") })
