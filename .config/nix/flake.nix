c141b97c37b1de0ffc31982e41e75d2393591f79
iz7n <48165199+iz7n@users.noreply.github.com>
2024-12-10 00:48:08 -0500

    feat: add darwin-nix config

---
+++ .config/nix/flake.nix
@@ -0,0 +1,77 @@
+{
+  description = "Example nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages = with pkgs; [
+        curl
+        wget
+        cmake
+        fish
+        starship
+        git
+        delta
+        fzf
+        fd
+        ripgrep
+        tmux
+        gitmux
+        sesh
+        neovim
+        ansible
+        fortune
+        ffmpeg
+        jq
+        lazygit
+        eza
+        bat
+        zoxide
+        yadm
+        yt-dlp
+        tldr
+        hexyl
+        darwin.trash
+        gum
+        stripe-cli
+        jdk
+        sqlite
+      ];
+
+      fonts.packages = with pkgs; [
+        nerd-fonts.fira-code
+      ];
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#m1
+    darwinConfigurations."m1" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+  };
+}
