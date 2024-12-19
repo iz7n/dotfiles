79c7997d91c7e6c2d7022a9d004a4bf03cb974b7
iz7n <48165199+iz7n@users.noreply.github.com>
2024-12-19 00:17:43 -0500

    chore: fmt oil file

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,18 +1,19 @@
 return {
-    "stevearc/oil.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        local oil = require("oil")
-        oil.setup({
-            keymaps = {
-                ["<C-h>"] = false,
-                ["<M-h>"] = "actions.select_split",
-            },
-            view_options = {
-                show_hidden = true,
-            },
-        })
+	"stevearc/oil.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local oil = require("oil")
+		oil.setup({
+			watch_for_changes = true,
+			keymaps = {
+				["<C-h>"] = false,
+				["<M-h>"] = "actions.select_split",
+			},
+			view_options = {
+				show_hidden = true,
+			},
+		})
 
-        vim.keymap.set("n", "<C-n>", ":Oil<CR>", { desc = "Open parent directory" })
-    end,
+		vim.keymap.set("n", "<C-n>", ":Oil<CR>", { desc = "Open parent directory" })
+	end,
 }
